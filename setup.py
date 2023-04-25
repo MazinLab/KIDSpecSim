93d0c0f101433100bda369cee48a3ff4a4a673e5
crystalkim0 <crystalkim@ucsb.edu>
2023-04-25 12:58:13 -0700

    Update setup.py with metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from Cython.Build import cythonize
 import numpy as np
 
 setup(
-    ext_modules=cythonize("sortarray.pyx","filterphot.pyx"),
+    name='ucsbsim',
+    version='0.1',
+    author='MazinLab',
+    author_email='mazinlab@ucsb.edu',
+    description='The MKID Spectrometer Simulation',
+    readme='README.md',
+    packages=find_packages(),
+    keywords=['mkid', 'spectrometer', 'simulation', 'spectra', 'order sorting'],
+    classifiers=[
+        "Private :: Do Not Upload",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3"
+    ],
+    ext_modules=cythonize("sortarray.pyx", "filterphot.pyx"),
     include_dirs=[np.get_include()]
 )
