dd3ca7857c90ded1bdbe65b2c5279aba73c25b97
crystalkim0 <crystalkim@ucsb.edu>
2023-04-28 10:27:22 -0700

    +docstring

--- ucsbsim/filterphot.pyx
+++ ucsbsim/filterphot.pyx
@@ -6,6 +6,11 @@ import numpy as np
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def mask_deadtime(times, deadtime):
+    """
+    :param times: arrival times of photons
+    :param deadtime: deadtime between initial trigger and detecting additional photons
+    :return: photons not in another's deadtime
+    """
     n = times.shape[0]
     keep = np.ones(n, bool)
     lim = times[0] + deadtime
