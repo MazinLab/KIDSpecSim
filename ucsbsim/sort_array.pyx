66eb338a3982d23824e66f4cc58d927d962e2d1b
crystalkim0 <crystalkim@ucsb.edu>
2023-04-28 10:31:54 -0700

    +docstring

--- ucsbsim/sort_array.pyx
+++ ucsbsim/sort_array.pyx
@@ -6,4 +6,9 @@ import numpy as np
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def sort(array, sorting_index):
+    """
+    :param array: some ndarray
+    :param sorting_index: how to sort the array
+    :return: sorted array
+    """
     return np.take_along_axis(array, sorting_index, axis=0)
