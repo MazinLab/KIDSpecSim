bdf15da21446f1f321baae97cc829c2564be624f
crystalkim0 <crystalkim@ucsb.edu>
2023-05-01 17:07:58 -0700

    Update engine.py
    added methods from fit.py

--- ucsbsim/engine.py
+++ ucsbsim/engine.py
@@ -15,7 +15,7 @@ import sortarray
 class Engine:
     def __init__(self, spectrograph: SpectrographSetup):
         self.spectrograph = spectrograph
-        print("\nConfigured engine for convolving spectrum and drawing photons.")
+        print("\nConfigured engine for convolving, drawing photons, and fitting.")
 
     def optically_broaden(self, wave, flux: u.Quantity, axis: int = 1):
         """
@@ -386,3 +386,48 @@ class Engine:
             t_photons.append(np.random.uniform(0, 1, size=n) * exptime)
         print("Completed photon draw, obtained random arrival times and wavelengths for individual photons.")
         return t_photons, l_photons
+
+    def gauss(self, x, mu, sig, A):
+        """
+        :param x: wavelength
+        :param mu: mean
+        :param sig: sigma
+        :param A: amplitude
+        :return: value of the Gaussian with those parameters at the wavelength
+        """
+        return (A * np.exp(- (x - mu) ** 2. / (2. * sig ** 2.))).T
+
+    def gauss_summed(self, x, *args):
+        """
+        :param x: wavelength range
+        :param args: mu, sig, and A for each order Gaussian, as (mu_1, mu_2, ..., sig_1, sig_2, ..., A_1, A_2, ...)
+        :return: sum of all Gaussian functions on the order axis
+        """
+        n = len(args)
+        if n % 3 != 0:
+            raise ValueError("You must include a mean, sigma, and A for each Gaussian.")
+        mu = np.array(args[:int(n / 3)])
+        sig = np.array(args[int(n / 3):int(2 * n / 3)])
+        A = np.array(args[int(2 * n / 3):])
+        return np.sum(self.gauss(x[:, None], mu, sig, A), axis=0)
+
+    def quad_formula(self, a, b, c):
+        """
+        :return: positive quadratic formula result for ax^2 + bx + c = 0
+        """
+        return (-b + np.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+
+    def gauss_intersect(self, mu, sig, A):
+        """
+        :param mu: means of Gaussians in order
+        :param sig: sigmas of Gaussians in order
+        :param A: amplitudes of Gaussians in order
+        :return: analytic calculation of the intersection point between adjacent 1D Gaussian functions
+        """
+        n = len(mu)
+        if n != len(sig) or n != len(A):
+            raise ValueError("mu, sig, and A must all be the same size.")
+        a = [1 / sig[i] ** 2 - 1 / sig[i + 1] ** 2 for i in range(n - 1)]
+        b = [2 * mu[i + 1] / sig[i + 1] ** 2 - 2 * mu[i] / sig[i] ** 2 for i in range(n - 1)]
+        c = [(mu[i] / sig[i]) ** 2 - (mu[i + 1] / sig[i + 1]) ** 2 - 2 * np.log(A[i] / A[i + 1]) for i in range(n - 1)]
+        return self.quad_formula(np.array(a), np.array(b), np.array(c))
