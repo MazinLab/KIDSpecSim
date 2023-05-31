b534348e7c165e455200a7a55542c6d1cbf24c90
crystalkim0 <crystalkim@ucsb.edu>
2023-05-31 14:29:49 -0700

    Create sortarray.pyx

---
+++ ucsbsim/sortarray.pyx
@@ -0,0 +1,14 @@
+cimport cython
+cimport numpy as np
+import numpy as np
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def sort(array, sorting_index):
+    """
+    :param array: some ndarray
+    :param sorting_index: some other index array by which to sort the array
+    :return: sorted array
+    """
+    return np.take_along_axis(array, sorting_index, axis=0)
