563b690473f70c7dfa3f398dc8547434bfdf4d9d
crystalkim0 <crystalkim@ucsb.edu>
2023-07-14 18:13:42 -0700

    Update msf.py

--- ucsbsim/msf.py
+++ ucsbsim/msf.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class MKIDSpreadFunction:
     def __init__(self, bin_edges=None, cov_matrix=None, orders=None, filename: str = '', sim_settings=None):
         """
-        :param bin_edges: n_ord+1 by n_pix array of wavelength bins
+        :param bin_edges: n_ord+1 by n_pix array of wavelength or phase bins
         :param cov_matrix: n_ord by n_ord by n_pix array of covariance fractions belonging to another order
         :param orders: list or 1D array of orders in spectrograph
         :param str filename: where the MKIDSpreadFunction exists or where to save a new file
@@ -15,7 +15,8 @@ class MKIDSpreadFunction:
         if filename:
             self._load()
         else:
-            assert bin_edges.any() and cov_matrix.any() and orders.any(), 'Must provide all other arguments if not specifying file.'
+            assert bin_edges.any() and cov_matrix.any() and orders.any(), \
+                'Must provide all other arguments if not specifying file.'
             self.bin_edges = bin_edges
             self.cov_matrix = cov_matrix
             self.orders = orders
@@ -45,9 +46,3 @@ class MKIDSpreadFunction:
             fig, ax = plt.subplots(1, 1, figsize=(8.5, 11), dpi=300)
         # add more
 
-
-if __name__ == '__main__':
-    msf = MKIDSpreadFunction(bin_edges=np.ones([6, 2048]), cov_matrix=np.ones([5, 5, 2048]), orders=[5, 6, 7, 8, 9],
-                             sim_settings=['blah'])
-    msf.save('testing.npz')
-    msf_1 = MKIDSpreadFunction(filename='testing.npz')
