485d62381531966341b495cb611e9ee8f5c22806
crystalkim0 <crystalkim@ucsb.edu>
2024-02-07 22:58:57 -0800

    Update mkidspec.py

--- ucsbsim/scripts/mkidspec.py
+++ ucsbsim/scripts/mkidspec.py
@@ -29,7 +29,7 @@ def parse():
     parser = argparse.ArgumentParser(description='MKID Spectrograph Data Reduction')
 
     # optional script args:
-    parser.add_argument('--outdir', default='testfiles/outdir', type=str, help='Directory for the output files.')
+    parser.add_argument('--outdir', default='../testfiles/outdir', type=str, help='Directory for the output files.')
     parser.add_argument('--plot', action='store_true', default=False, type=bool,
                         help='If passed, indicates that intermediate plots will be shown.')
     parser.add_argument('--resid_map', default=np.arange(2048, dtype=int) * 10 + 100,
@@ -39,28 +39,30 @@ def parse():
     parser.add_argument('--minw', default=400, type=float, help='The minimum wavelength of the spectrograph in nm.')
     parser.add_argument('--maxw', default=800, type=float, help='The maximum wavelength of the spectrograph in nm.')
     parser.add_argument('--npix', default=2048, type=int, help='The number of pixels in the MKID detector.')
-    parser.add_argument('--pixsize', default=20,
+    parser.add_argument('--pixsize', default=20, type=float,
                         help='The length of the MKID pixel in the dispersion direction in um.')
     parser.add_argument('-R0', default=15, type=float, help='The spectral resolution at the maximum wavelength.')
-    parser.add_argument('-l0', default=800,
-                        help="The longest wavelength in nm. Can be float or 'same' to be equal to 'maxwave' arg.")
-    parser.add_argument('--osamp', default=10,
+    parser.add_argument('-l0', default='same',
+                        help="The longest wavelength in nm. Can be float or 'same' to be equal to 'maxw' arg.")
+    parser.add_argument('--osamp', default=10, type=int,
                         help='The number of samples to use for the smallest pixel dlambda during convolution.')
     parser.add_argument('--nsig', default=3, type=float,
                         help='The number of sigma to use for Gaussian during convolution.')
     parser.add_argument('--alpha', default=28.3, type=float, help='Angle of incidence on the grating in degrees.')
-    parser.add_argument('--beta', default=34.7, type=float, help='Reflectance angle at the central pixel in degrees. ')
+    parser.add_argument('--beta', default='littrow', type=float,
+                        help='Reflectance angle at the central pixel in degrees.'
+                             'Can be a number or "littrow" to be equal to alpha.')
     parser.add_argument('--delta', default=63, type=float, help='Blaze angle in degrees.')
     parser.add_argument('-d', '--groove_length', default=((1 / 316) * u.mm).to(u.nm).value, type=float,
                         help='The groove length d, or distance between slits, of the grating in nm.')
     parser.add_argument('--m0', default=4, type=int, help='The initial order, at the longer wavelength end.')
-    parser.add_argument('--m_max', default=8, type=int, help='The final order, at the shorter wavelength end.')
+    parser.add_argument('--m_max', default=7, type=int, help='The final order, at the shorter wavelength end.')
     parser.add_argument('-ppre', '--pixels_per_res_elem', default=2.5, type=float,
                         help='Number of pixels per spectral resolution element for the spectrograph.')
     parser.add_argument('--focallength', default=300, type=float, help='The focal length of the detector in mm.')
 
     # optional MSF args:
-    parser.add_argument('--msf', default='testfiles/flat.h5', type=str,
+    parser.add_argument('--msf', default='../testfiles/flat.h5', type=str,
                         help='Directory/name of the flat/blackbody spectrum photon table .h5 file OR'
                              'Directory/name of the complete MKID Spread Function .npz file.'
                              'Pass "False" to disable this step.')
@@ -68,15 +70,15 @@ def parse():
                         help='Start and stop of range for phase histogram.')
 
     # optional wavecal args:
-    parser.add_argument('--wavecal', default='testfiles/emission.h5', type=str,
+    parser.add_argument('--wavecal', default='../testfiles/emission.h5', type=str,
                         help='Directory/name of the emission lamp spectrum photon table .h5 file OR'
                              'Directory/name of the order-sorted emission lamp spectrum FITS file OR'
                              'Directory/name of the complete wavelength calibration solution .npz file.'
                              'Pass "False" to disable this step.')
     parser.add_argument('--elem', default='hgar', type=str,
                         help="Emission lamp element in use, i.e., 'hgar' for Mercury-Argon.")
-    parser.add_argument('--orders', default=[8, 7, 6, 5, 4], type=list,  # TODO which orders are actually incident
-                        help="Orders to be used. Useful if you only want to fit 1 order at a time.")
+    parser.add_argument('--orders', default=[7, 6, 5, 4], type=list,
+                        help="Orders to be used. Useful if you only want to 1, 2, 3 orders, etc.")
     parser.add_argument('--degree', default=4, type=int, help="Polynomial degree to use in wavecal.")
     parser.add_argument('--iters', default=5, type=int,
                         help="Number of iterations to loop through for identifying and discarding lines.")
@@ -91,7 +93,7 @@ def parse():
                         help="Return a '1D' (pixel direction) or '2D' (pixel+order directions) fitting solution.")
 
     # optional observation args:
-    parser.add_argument('--extract', default='testfiles/phoenix.h5', type=str,
+    parser.add_argument('--extract', default='../testfiles/phoenix.h5', type=str,
                         help='Directory/name of the on-sky observation spectrum photon table .h5 file OR'
                              'Directory/name of the order-sorted observation spectrum FITS file.'
                              'Pass "False" to disable this step.')
@@ -107,7 +109,7 @@ if __name__ == "__main__":
 
     os.makedirs(name=f'{args.outdir}', exist_ok=True)
 
-    logging.basicConfig(filename=f'{args.outdir}/{now.strftime("%Y%m%d_%H%M%S")}.log',
+    logging.basicConfig(filename=f'{args.outdir}/mkidspec_{now.strftime("%Y%m%d_%H%M%S")}.log',
                         format='%(levelname)s:%(message)s', level=logging.INFO)
 
     # packing up the simulation values into a class
