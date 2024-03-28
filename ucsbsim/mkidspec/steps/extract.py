iterative bleeding subtraction, fixed covariance bug

ucsbsim/mkidspec/steps/extract.py
@@ -28,14 +28,16 @@ def extract(
     orders = wavecal_file['orders']
 
     spectrum = fits.open(f'{obs_fits}', mode='update')
-    obs_flux = np.array([np.array(spectrum[1].data[n]) for n, i in enumerate(orders)])
+    obs_flux_corr = np.array([np.array(spectrum[1].data[n]) for n, i in enumerate(orders)])
     err_n = np.array([np.array(spectrum[2].data[n]) for n, i in enumerate(orders)])
     err_p = np.array([np.array(spectrum[3].data[n]) for n, i in enumerate(orders)])
     guess_wave = np.array([np.array(spectrum[4].data[n]) for n, i in enumerate(orders)])
+    obs_flux = np.array([np.array(spectrum[5].data[n]) for n, i in enumerate(orders)])
     hdu_list = fits.HDUList([spectrum[0],
                              spectrum[1],
                              spectrum[2],
                              spectrum[3],
+                             spectrum[5],
                              fits.BinTableHDU(Table(wavecal), name='Wavecal')])
     hdu_list.writeto(obs_fits, output_verify='ignore', overwrite=True)
     logging.info(f'The FITS file has been updated with the wavecal: {obs_fits}.')
@@ -56,8 +58,6 @@ def extract(
         plt.show()
 
         # plot with errors
-        obs_flux_corr = obs_flux + (err_p - err_n)
-        obs_flux_corr[obs_flux_corr < 0] = 0
         fig, ax = plt.subplots(1, 2, sharey=True, figsize=(10, 5))
         axes = ax.ravel()
 

ucsbsim/mkidspec/steps/fitmsf.py
@@ -430,8 +430,8 @@ def cov_from_params(params, model, nord, order_edges, valid_idx, x_phases, **fit
         suppress_model = interp.InterpolatedUnivariateSpline(x=x_phases, y=suppress_model, k=1, ext=1)
         suppress_model_sum = np.array([
             suppress_model.integral(order_edges[i], order_edges[i + 1]) for i in range(len(order_edges) - 1)])
-        cov[valid_idx, k] = np.nan_to_num((model_sum - suppress_model_sum) / model_sum)
-        if np.array_equal(cov[valid_idx, k], np.zeros([nord])):
+        cov[k, valid_idx] = np.nan_to_num((model_sum - suppress_model_sum) / model_sum)
+        if np.array_equal(cov[k, valid_idx], np.zeros([nord])):
             cov[k, k] = 1
     cov[cov < 0] = 0
     return cov
@@ -519,15 +519,15 @@ def fitmsf(
     full_pix = []
     opt_param_all = []
 
-    redchi_val = 30  # TODO remove after debug
+    redchi_val = 50  # TODO remove after debug
     xtol = 1e-4
-    #pixels = [1499]
+    #pixels = range(900, 921)
 
     leg_e = Legendre(coef=(0, 0, 0), domain=np.array(bin_range))
 
     # do the non-linear least squares fit:
     for p in pixels:
-        #print(p)
+        # print(p)
         leg_s = Legendre(coef=(0, 0, 0), domain=[energy[0, p] / energy[-1, p], 1])
 
         peaks, _ = sig.find_peaks(x=bin_counts[:, p], distance=int(0.095 * bins), height=snr**2)
@@ -560,58 +560,6 @@ def fitmsf(
                         else:
                             peaks_nord = np.insert(peaks_nord, o, int((peaks_sort[o-1] + peaks_sort[o])/2))
                     missing_ord = item[1]
-                    # if p <= 346 and len(peaks_sort) == 3:  # 1 order missing
-                    #     peaks_nord = np.array([
-                    #         peaks_sort[0],
-                    #         peaks_sort[1],
-                    #         int((peaks_sort[1] + peaks_sort[2]) / 2),
-                    #         peaks_sort[2]]
-                    #     )
-                    #     missing_ord = [2]
-                    # elif p <= 1300 and len(peaks_sort) == 2:  # 2 orders missing
-                    #     peaks_nord = np.array([
-                    #         int(peaks_sort[0] - (peaks_sort[1] - peaks_sort[0]) / 2),
-                    #         peaks_sort[0],
-                    #         int((peaks_sort[0] + peaks_sort[1]) / 2),
-                    #         peaks_sort[1]]
-                    #     )
-                    #     missing_ord = [0, 2]
-                    # elif 346 < p <= 1300 and len(peaks_sort) == 3:  # 1 order missing
-                    #     peaks_nord = np.array([
-                    #         int(peaks_sort[0] - (peaks_sort[1] - peaks_sort[0]) / 2),
-                    #         peaks_sort[0],
-                    #         peaks_sort[1],
-                    #         peaks_sort[2]]
-                    #     )
-                    #     missing_ord = [0]
-                    # elif 1680 < p < 1800 and len(peaks_sort) == 3:
-                    #     # weird zone with flip flop between order 4 and 5
-                    #     peaks, _ = sig.find_peaks(bin_counts[:, p], distance=int(0.25 * n_bins), height=snr**2)
-                    #     idx = np.argsort(bin_counts[peaks, p])[::-1]
-                    #     peaks_sort = np.sort(peaks[idx])
-                    #     peaks_nord = np.array([
-                    #         peaks_sort[0],
-                    #         int((peaks_sort[0] + peaks_sort[1]) / 2),
-                    #         peaks_sort[1],
-                    #         int(peaks_sort[1] + (peaks_sort[1] - peaks_sort[0]) / 2)]
-                    #     )
-                    #     missing_ord = [1, 3]
-                    # elif p > 1300 and len(peaks_sort) == 3:  # 1 order missing
-                    #     peaks_nord = np.array([
-                    #         peaks_sort[0],
-                    #         int((peaks_sort[0] + peaks_sort[1]) / 2),
-                    #         peaks_sort[1],
-                    #         peaks_sort[2]]
-                    #     )
-                    #     missing_ord = [1]
-                    # elif p > 1300 and len(peaks_sort) == 2:  # 2 orders missing
-                    #     peaks_nord = np.array([
-                    #         peaks_sort[0],
-                    #         int((peaks_sort[0] + peaks_sort[1]) / 2),
-                    #         peaks_sort[1],
-                    #         int(peaks_sort[1] + (peaks_sort[1] - peaks_sort[0]) / 2)]
-                    #     )
-                    #     missing_ord = [1, 3]
             if missing_ord is None:  # e.g.: only 1 peak was found
                 logging.info(f'Pixel {p} will default to simulation guess.')
                 peaks_nord = [gen.nearest_idx(bin_centers, sim_phase[i, p]) for i in range(nord)]

ucsbsim/mkidspec/steps/ordersort.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 import matplotlib.pyplot as plt
 import time
 from datetime import datetime as dt
@@ -67,19 +68,30 @@ def ordersort(
     for j in detector.pixel_indices:  # binning photons by MSF bins edges
         spec[:, j], _ = np.histogram(photons_pixel[j], bins=msf.bin_edges[:, j])
 
-    # uncorrected errors
-    err_p = np.array([[int(np.sum(msf.cov_matrix[i, :, j] * spec[i, j]) -
-                       msf.cov_matrix[i, i, j] * spec[i, j]) for j in detector.pixel_indices] for i in range(nord)])
-    err_n = np.array([[int(np.sum(msf.cov_matrix[:, i, j] * spec[:, j]) -
-                           msf.cov_matrix[i, i, j] * spec[i, j]) for j in detector.pixel_indices] for i in range(nord)])
+    spec_unfixed = deepcopy(spec)
+
+    err_p = np.zeros([nord, sim.npix])
+    err_n = np.zeros([nord, sim.npix])
+    count_order = np.argsort(spec, axis=0)
+    for j in detector.pixel_indices:
+        for i in count_order[::-1][:, j]:
+            err_indv = np.delete(msf.cov_matrix[i, :, j] * spec[i, j], i)
+            err_n[:, j] += np.insert(err_indv, i, 0)
+            err_p[i, j] = np.sum(err_indv)
+            spec[:, j] = np.around(np.insert(np.delete(spec[:, j], i) - err_indv, i, spec[i, j] + err_p[i, j])).astype(int)
+    spec[spec < 0] = 0
+    err_p = np.around(err_p).astype(int)
+    err_n = np.around(err_n).astype(int)
 
     # saving extracted and unblazed spectrum to file
     fits_file = f'{outdir}/{filename}.fits'
     hdu_list = fits.HDUList([fits.PrimaryHDU(),
-                             fits.BinTableHDU(Table(spec), name='Spectrum'),
-                             fits.BinTableHDU(Table(err_n), name='- Errors'),
-                             fits.BinTableHDU(Table(err_p), name='+ Errors'),
-                             fits.BinTableHDU(Table(lambda_pixel.to(u.Angstrom)), name='Wave Range')])
+                             fits.BinTableHDU(Table(spec), name='Corrected Spectrum'),
+                             fits.BinTableHDU(Table(err_p), name='- Errors'),
+                             fits.BinTableHDU(Table(err_n), name='+ Errors'),
+                             fits.BinTableHDU(Table(lambda_pixel.to(u.Angstrom)), name='Wave Range'),
+                             fits.BinTableHDU(Table(spec_unfixed), name='Uncorrected Spectrum')])
+
     hdu_list.writeto(fits_file, output_verify='ignore', overwrite=True)
     logging.info(f'The extracted spectrum with its errors has been saved to {fits_file}.')
 
@@ -87,21 +99,16 @@ def ordersort(
         spectrum = fits.open(fits_file)
 
         # plot the spectrum unblazed with the error band:
-        fig2, ax2 = plt.subplots(int(np.ceil(nord/2)), 2, figsize=(30, int(10*nord)), sharex=True)
-        axes2 = ax2.ravel()
+        fig, ax = plt.subplots(int(np.ceil(nord/2)), 2, figsize=(15, int(5*nord)), sharex=True)
+        axes = ax.ravel()
         for i in range(nord):
-            spec_w_merr = np.array(spectrum[1].data[i]) - np.array(spectrum[2].data[i])
-            spec_w_perr = np.array(spectrum[1].data[i]) + np.array(spectrum[3].data[i])
-            spec_w_merr[spec_w_merr < 0] = 0
-            axes2[i].grid()
-            axes2[i].fill_between(detector.pixel_indices, spec_w_merr, spec_w_perr, edgecolor='r', facecolor='r',
-                                  linewidth=0.5)
-            axes2[i].plot(detector.pixel_indices, spectrum[1].data[i])
-            axes2[i].set_title(f'Order {7 - i}')
-        axes2[-1].set_xlabel("Pixel Index")
-        axes2[-2].set_xlabel("Pixel Index")
-        axes2[0].set_ylabel('Photon Count')
-        axes2[2].set_ylabel('Photon Count')
+            axes[i].grid()
+            axes[i].plot(detector.pixel_indices, spectrum[1].data[i])
+            axes[i].set_title(f'Order {spectro.orders[::-1][i]}')
+        axes[-1].set_xlabel("Pixel Index")
+        axes[-2].set_xlabel("Pixel Index")
+        axes[0].set_ylabel('Photon Count')
+        axes[2].set_ylabel('Photon Count')
         plt.suptitle(f'Sorted spectrum with error band')
         plt.tight_layout()
         plt.show()